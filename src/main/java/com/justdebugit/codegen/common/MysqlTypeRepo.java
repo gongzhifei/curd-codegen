842b20aaba594dd0716c9520818eb23c221784ff
justdebugit <justdebugit@gmail.com>
2017-08-01 17:29:19 +0800

    增加类型

--- src/main/java/com/justdebugit/codegen/common/MysqlTypeRepo.java
+++ src/main/java/com/justdebugit/codegen/common/MysqlTypeRepo.java
@@ -12,17 +12,24 @@ public class MysqlTypeRepo {
     typeMap.put("bigint", "java.lang.Long");
     typeMap.put("tinyint", "java.lang.Integer");
     typeMap.put("int", "java.lang.Integer");
+    typeMap.put("mediumint", "java.lang.Integer");
+    typeMap.put("float", "java.lang.Double");
+    typeMap.put("double", "java.lang.Double");
     typeMap.put("text", "java.lang.String");
+    typeMap.put("tinytext", "java.lang.String");
+    typeMap.put("longtext", "java.lang.String");
+    typeMap.put("mediumtext", "java.lang.String");
     typeMap.put("varchar", "java.lang.String");
     typeMap.put("char", "java.lang.String");
     typeMap.put("timestamp", "java.util.Date");
     typeMap.put("date", "java.util.Date");
     typeMap.put("datetime", "java.util.Date");
+    typeMap.put("time", "java.util.Date");
     typeMap.put("smallint", "java.lang.Integer");
   }
 
   public static String getTypeName(String mysqlType) {
-    String type =  typeMap.get(mysqlType);
+    String type =  typeMap.get(mysqlType.toLowerCase());
     if (type == null) {
       throw new IllegalArgumentException("Can not find mysqlType mapping " + mysqlType);
     }
